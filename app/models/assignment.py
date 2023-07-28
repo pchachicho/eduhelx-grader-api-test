c2f377a4ecfb7d1b34faef4d0832ad2cc6953812
Griffin Roupe <groupe@renci.org>
2023-07-28 14:47:00 -0400

    Change assigment duration back to due date. Change deferred date to deferred time interval

--- app/models/assignment.py
+++ app/models/assignment.py
@@ -19,14 +19,8 @@ class AssignmentModel(Base):
     directory_path = Column(Text, nullable=False)
     created_date = Column(DateTime(timezone=True), default=func.current_timestamp())
     available_date = Column(DateTime(timezone=True))
-    assignment_duration = Column(Interval)
+    due_date = Column(DateTime(timezone=True))
     last_modified_date = Column(DateTime(timezone=True), default=func.current_timestamp())
-
-    @hybrid_property
-    def due_date(self):
-        if self.available_date is None or self.assignment_duration is None:
-            return None
-        return self.available_date + self.assignment_duration
 
     def _get_extra_time_model(self, db: Session, onyen: str):
         extra_time_model = db.query(ExtraTimeModel) \
@@ -43,27 +37,28 @@ class AssignmentModel(Base):
         if self.available_date is None: return None
 
         extra_time_model = self._get_extra_time_model(db, onyen)
-        if extra_time_model is not None:
-            if extra_time_model.deferred_date is not None:
-                return extra_time_model.deferred_date
+        deferred_time = extra_time_model.deferred_time if extra_time_model is not None else timedelta(0)
         
-        return self.available_date
+        return self.available_date + deferred_time
 
     # The due date for a specific student, considering extra_time
     def get_adjusted_due_date(self, db: Session, onyen: str):
-        available_date = self.get_adjusted_available_date(db, onyen)
-        if available_date is None or self.assignment_duration is None:
-            return None
+        if self.due_date is None: return None
 
         extra_time_model = self._get_extra_time_model(db, onyen)
         # If a student does not have any extra time allotted for the assignment,
         # allocate them a timedelta of 0.
-        extra_time = extra_time_model.extra_time if extra_time_model is not None else timedelta(0)
+        if extra_time_model is not None:
+            deferred_time = extra_time_model.deferred_time
+            extra_time = extra_time_model.extra_time
+        else:
+            deferred_time = timedelta(0)
+            extra_time = timedelta(0)
 
-        return available_date + self.assignment_duration + extra_time
+        return self.due_date + deferred_time + extra_time
     
     def get_is_released(self):
-        return self.available_date is not None and self.assignment_duration is not None
+        return self.available_date is not None and self.due_date is not None
 
     def get_is_available_for_student(self, db: Session, onyen: str) -> bool:
         if not self.get_is_released(): return False
