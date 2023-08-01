21d03895953fb0f163c545921f5709a5fe2b9974
Griffin Roupe <groupe@renci.org>
2023-07-31 20:40:58 -0400

    Add join_date/exit_date to student

--- app/models/student.py
+++ app/models/student.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Sequence, Integer, Text, Interval
+from sqlalchemy import Column, Sequence, Integer, Text, Interval, DateTime, func
 from app.database import Base
 
 class StudentModel(Base):
@@ -10,3 +10,5 @@ class StudentModel(Base):
     last_name = Column(Text, nullable=False)
     professor_onyen = Column(Text, nullable=False)
     base_extra_time = Column(Interval, server_default="0")
+    join_date = Column(DateTime(timezone=True), server_default=func.current_timestamp())
+    exit_date = Column(DateTime(timezone=True))
