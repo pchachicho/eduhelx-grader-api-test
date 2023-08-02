4b97e2856dae34fa5a72caf4e32bd53188a5e2b0
Griffin Roupe <groupe@renci.org>
2023-08-02 13:04:51 -0400

    Add instructor table and make related adjustments

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .student import StudentModel
 from .submission import SubmissionModel
 from .assignment import AssignmentModel
+from .instructor import InstructorModel
 from .extra_time import ExtraTimeModel
 from .course import CourseModel
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -3,3 +3,4 @@ from .submission import SubmissionSchema
 from .assignment import AssignmentSchema, StudentAssignmentSchema
 from .extra_time import ExtraTimeSchema
 from .course import CourseSchema
+from .instructor import InstructorSchema
