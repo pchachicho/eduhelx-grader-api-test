7fc9ff12c8ba4846a2ee867f2522111709d033b6
Griffin Roupe <groupe@renci.org>
2023-08-10 18:51:34 -0400

    Fix student inheritance

--- app/models/user/student.py
+++ app/models/user/student.py
@@ -4,10 +4,8 @@ from app.database import Base
 class StudentModel(Base):
     __tablename__ = "student"
 
-    id = Column(Integer, Sequence("student_id_seq"), primary_key=True, autoincrement=True, index=True)
-
     base_extra_time = Column(Interval, server_default="0")
     join_date = Column(DateTime(timezone=True), server_default=func.current_timestamp())
     exit_date = Column(DateTime(timezone=True))
     
-    mapper_args = {"__polymorphic_identity": "student"}
+    __mapper_args__ = {"polymorphic_identity": "student"}
