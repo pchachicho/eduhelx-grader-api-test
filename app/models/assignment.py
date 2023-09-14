140ba45099cfa6137afec8cdbd25d719d5abe3ed
Harit <hpatel@renci.org>
2023-09-14 15:16:17 -0400

    Update assignment model to have unique name

--- app/models/assignment.py
+++ app/models/assignment.py
@@ -15,7 +15,7 @@ class AssignmentModel(Base):
     __tablename__ = "assignment"
 
     id = Column(Integer, Sequence("assignment_id_seq"), primary_key=True, autoincrement=True, index=True)
-    name = Column(Text, nullable=False)
+    name = Column(Text, nullable=False, unique=True)
     directory_path = Column(Text, nullable=False)
     created_date = Column(DateTime(timezone=True), server_default=func.current_timestamp())
     available_date = Column(DateTime(timezone=True))
