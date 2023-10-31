c81604f9d82ef35c39a684345548a3d5a6fac3ea
Harit <hpatel@renci.org>
2023-10-31 14:21:30 -0400

    Fix error in starting API

--- start.py
+++ start.py
@@ -46,8 +46,8 @@ def main(host, port, reload):
     # Start the application
     uvicorn_args = ["uvicorn", "app.main:app", "--host", host, "--port", port]
     if reload: uvicorn_args.append("--reload")
-    subprocess.run(uvicorn_args)
-    # uvicorn.run(app, host=host, port=int(port), reload=reload)
+    # subprocess.run(uvicorn_args)
+    uvicorn.run(app, host=host, port=int(port), reload=reload)
 
 
 if __name__ == "__main__":
