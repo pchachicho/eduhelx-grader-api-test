6d000c7bd569762cdea9db60bbac3966c1221be9
Harit <hpatel@renci.org>
2024-03-29 15:23:28 -0400

    .

--- app/services/canvas-test.py
+++ app/services/canvas-test.py
@@ -4,7 +4,7 @@ import requests
 app = FastAPI()
 
 # Assuming you have stored your Canvas LMS API key securely
-CANVAS_API_KEY = "7006~RMJbYVaRDn3SeKt3kekSHJ1RaVk5oLbmDjkxg650f2rQ6MXwHoeAl3xiWO2rtnzZ"
+CANVAS_API_KEY = ""
 CANVAS_API_URL = "https://uncch.instructure.com/api/v1"
 
 headers = {
