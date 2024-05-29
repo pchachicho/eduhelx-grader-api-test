494efdafdc7aeb52ff13a65e4a908cb8fff94d48
Harit <hpatel@renci.org>
2024-05-29 12:48:57 -0400

    remove exception from user

--- app/core/exceptions/user.py
+++ app/core/exceptions/user.py
@@ -20,16 +20,6 @@ class UserNotFoundException(CustomException):
     error_code = "USER__NOT_FOUND"
     message = "user not found"
 
-class NoStudentsFetchedException(CustomException):
-    code = 404
-    error_code = "USER__NO_STUDENTS_FOUND"
-    message = "no students found"
-
-class NotAStudentException(CustomException):
-    code = 403
-    error_code = "USER__NOT_A_STUDENT"
-    message = "user is not a student"
-
 class NotAnInstructorException(CustomException):
     code = 403
     error_code = "USER__NOT_AN_INSTRUCTOR"
