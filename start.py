c3ec195c164a01a97d1c79e613663e593bf90d74
Hoid <tylerlcheek@gmail.com>
2024-08-02 13:38:03 -0400

    Fix logic for startup

--- start.py
+++ start.py
@@ -52,14 +52,28 @@ def main(host, port, reload, workers):
     except ValueError as e:
         print(str(e))
 
-    # Start the application
+    # Start the application (old)
     uvicorn_args = ["uvicorn", "app.main:app", "--host", host, "--port", port]
     if reload: uvicorn_args.append("--reload")
     if workers: uvicorn_args.append("--workers")
-    if reload: workers = None
-    portInt = int(port)
     # subprocess.run(uvicorn_args)
-    uvicorn.run("app.main:app", host=host, port=portInt, reload=reload, workers=workers)
+
+    # Check inputs
+    portInt: int = None
+    workersInt: int = None
+    try:
+        if port: portInt = int(port)
+        if portInt is not None and portInt <= 0: raise ValueError()
+    except ValueError:
+        raise SystemExit("Error: Please use a positive integer value for the port.")
+    try:
+        if workers: workersInt = int(workers)
+        if workersInt is not None and workersInt <= 0: raise ValueError()
+    except ValueError:
+        raise SystemExit("Error: Please use a positive integer value for the number of workers.")
+    
+    # Start the application
+    uvicorn.run("app.main:app", host=host, port=portInt, reload=reload, workers=workersInt)
 
 
 if __name__ == "__main__":
@@ -70,7 +84,7 @@ if __name__ == "__main__":
     parser.add_argument_group('Production vs. Development', 'One of these options must be chosen. --reload is most useful when developing; never use it in production. Use --workers in production.')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-r", "--reload", action="store_true", help="Enable auto-reload.")
-    group.add_argument("-w", "--workers", help="Workers help")
+    group.add_argument("-w", "--workers", help="Enable the use of workers. This option cannot be used with --reload.")
     args = parser.parse_args()
     
     host = args.host
